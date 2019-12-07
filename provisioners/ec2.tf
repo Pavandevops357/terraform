5b27c103c1ba218dcbd2e42e6bff7fb5713f9282
Pavandevops357 <pavankalyan.anumula96@gmail.com>
2019-12-08 02:32:19 +0530

    student app

---
+++ provisioners/ec2.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "web" {
+  ami = "ami-02383135f96b2293e"
+  instance_type = "t2.micro"
+  vpc_security_group_ids["sg-03413ff0fcf074bb4", "sg-0fcc10a5254080161"]
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      user = "root"
+      password = "DevOps321"
+      host = self.public_ip
+      inline = [
+        "cd /tmp",
+        "git clone https://github.com/Pavandevops357/pkDevOps.git",
+        "sh /tmp/pkDevOps/student-app.sh",
+
+      ]
+    }
+  }
+}
