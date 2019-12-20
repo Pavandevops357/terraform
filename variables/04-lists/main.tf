547f3965af93648a39126e52fad9d88eed2ef662
Pavandevops357 <pavankalyan.anumula96@gmail.com>
2019-12-21 03:24:38 +0530

    printing the list of things

---
+++ variables/04-lists/main.tf
@@ -0,0 +1,10 @@
+variable "sample" {
+  type    = "list"
+  default = ["apple", "banana"]
+}
+
+resource "null_resource" "sample" {
+  provisioner "local-exec" {
+    command = "echo ${var.sample[0]}"
+  }
+}
