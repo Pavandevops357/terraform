a060c9c9724a50b1eabe3def2ce2e06b34b47dd7
Pavandevops357 <pavankalyan.anumula96@gmail.com>
2019-12-21 05:03:11 +0530

    command to run a ec2

--- outputs/main.tf
+++ outputs/main.tf
@@ -1,11 +1,5 @@
-variable "ami" {
-  default = "ami-02383135f96b2293e"
-}
-variable "instance_type" {
-  default = "t2.micro"
-}
-
 resource "aws_instance" "web" {
-  ami = var.ami
+  ami           = var.ami
   instance_type = var.instance_type
 }
+
