c3e07806d325edd67dd2578b706918b26fac874b
Pavandevops357 <pavankalyan.anumula96@gmail.com>
2019-12-21 04:29:08 +0530

    command to run a ec2  instance

---
+++ outputs/main.tf
@@ -0,0 +1,14 @@
+variable "ami" {
+  default = "ami-02383135f96b2293e"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+resource "aws_instance" "web" {
+  ami = var.ami
+  instance_type = "t2.micro"
+  tags {
+    name = "hello world"
+  }
+}
