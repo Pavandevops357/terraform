396167ea0c4ffe83513967b08eafe30d1c9449ba
Pavandevops357 <pavankalyan.anumula96@gmail.com>
2019-12-21 03:38:20 +0530

    printing the maps

---
+++ variables/05-maps/main.tf
@@ -0,0 +1,14 @@
+variable "sample" {
+  type    = "map"
+  default = {
+    course_name = "DevOps"
+    timings     = "6AMIST"
+    trainer     = "Raghu"
+  }
+}
+
+resource "null_resource" "sample" {
+  provisioner "local-exec" {
+    command = "echo Welcome to ${var.sample["course_name"]} Training, Timing is ${var.sample["timings"]} and Trainer is ${var.sample["trainer"]}"
+  }
+}
