efa72bfbf62e793302bac20858b572ce58aa1581
Pavandevops357 <pavankalyan.anumula96@gmail.com>
2019-12-21 02:49:53 +0530

    variables of terraform with string

--- variables/01-strings/main.tf
+++ variables/01-strings/main.tf
@@ -1,3 +1,8 @@
 variable "sample" {
   default = "hello-world" // value should be given in double coutes as it is a string
 }
+resource "null_resource" "sample" {
+  provisioner "local-exec" {
+    command = "echo ${var.sample}"
+  }
+}
